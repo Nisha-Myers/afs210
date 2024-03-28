efefd2bf3545f14db55dc547e821874963048bc4
unknown <equiliberalium89@gmail.com>
2024-03-27 21:40:51 -0400

    .

--- week4/hashing.py
+++ week4/hashing.py
@@ -5,15 +5,34 @@ class HashTable:
         self.data = [None] * self.size
 
     def hashfunction(self,key):
+         return key % self.size
         # Insert your hashing function code
 
     def rehash(self,key):
+             return key // self.size
         # Insert your secondary hashing function code
 
     def put(self,key,data):
+        hashvalue = self.hashfunction(key)
+        if self.data[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data
+            else:
+                nextslot = self.rehash(key)
+                if self.data[nextslot] == None:
+                    self.data[nextslot] = data
+                    self.slots[nextslot] = key
+                else:
+                        print("I'm so exhaustd...")
+
         # Insert your code here to store key and data 
 
     def get(self,key):
+        data = None
+        position = self.hashfunction(key)
         # Insert your code here to get data by key
 
     def __getitem__ (self,key):
@@ -25,7 +44,18 @@ class HashTable:
 
 H = HashTable()
 H[69] = 'A'
+H[66] = 'B'
+H[80] = 'C'
+H[35] = 'D'
+H[18] = 'E'
+H[52] = 'F'
+H[89] = 'G'
+H[70] = 'H'
+H[12] = 'I'
 # Store remaining input data
+print(H.slots)
+print(H.data)
+print(H[52])
 
 # print the slot values
 
